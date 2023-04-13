4eb1bc6e6f88ba38764ba988013c9e502754fc4e
thesamplepg <thesamplepg@gmail.com>
2023-04-13 17:35:55 +0600

    2

--- index.js
+++ index.js
@@ -108,8 +108,6 @@ app.post('/api/form', (req,res) => {
 
 const port = process.env.PORT || 5000;
 
-console.log(port)
-
 app.listen(port, (err) => {
     if(err) return console.log(err);
     console.log(`--[Server started ( Listening to ${port} )]--`);
